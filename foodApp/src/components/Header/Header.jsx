312a97cfd1fceae281880d279efa5b42d69c48d3
Ukinebo1 <perfectprecious2022@gmail.com>
2024-05-13 11:20:55 +0100

    Finish the nav bar and did the routing system to the various pages

---
+++ foodApp/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-content">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- foodApp/src/components/Navbar/Navbar.jsx
+++ foodApp/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 
 const Navbar = () => {
+  const [menu, setMenu]=useState("Menu")
   return (
     <div className='navbar'>
         <img src={assets.logo} alt="" className="logo" />
         <ul className="navbar-menu">
-            <li>Home</li>
-            <li>Menu</li>
-            <li>Mobile-app</li>
-            <li>Contact Us</li>
+            <li onClick={()=>setMenu("Home")} className={menu ==='Home'?'active':''}>Home</li>
+            <li onClick={()=>setMenu("Menu")} className={menu ==='Menu'?'active':''}>Menu</li>
+            <li onClick={()=>setMenu("Mobile-app")} className={menu ==='Mobile-app'?'active':''}>Mobile-app</li>
+            <li onClick={()=>setMenu("Contact Us")} className={menu ==='Contact Us'?'active':''}>Contact Us</li>
         </ul>
         <div className="navbar-right">
             <img src={assets.search_icon} alt="" />
