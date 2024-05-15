378b4f29158f2ed0567fe1602dab6269564d05ab
Ukinebo1 <perfectprecious2022@gmail.com>
2024-05-15 13:32:45 +0100

    created the cartpage and implemented the logic for adding and removing items, Also made all the pages so far responsive

--- foodApp/src/App.jsx
+++ foodApp/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
@@ -6,12 +6,17 @@ import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 // import FoodItem from "./components/FoodItem/FoodItem";
 import Footer from "./components/Footer/Footer";
+import LoginPopup from "./components/LoginPopup/LoginPopup";
 
 const App = () => {
+  const [showLogin, setShowLogin] =useState(false)
+
+
   return (
     <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin} />:<></>}
       <div className="app">
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
--- foodApp/src/components/Navbar/Navbar.jsx
+++ foodApp/src/components/Navbar/Navbar.jsx
@@ -1,25 +1,28 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
+import {Link} from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
   const [menu, setMenu]=useState("Menu")
   return (
+    
     <div className='navbar'>
-        <img src={assets.logo} alt="" className="logo" />
+        <Link to='/'><img src={assets.logo} alt="" className="logo" /></Link>
         <ul className="navbar-menu">
-            <li onClick={()=>setMenu("Home")} className={menu ==='Home'?'active':''}>Home</li>
-            <li onClick={()=>setMenu("Menu")} className={menu ==='Menu'?'active':''}>Menu</li>
-            <li onClick={()=>setMenu("Mobile-app")} className={menu ==='Mobile-app'?'active':''}>Mobile-app</li>
-            <li onClick={()=>setMenu("Contact Us")} className={menu ==='Contact Us'?'active':''}>Contact Us</li>
+          
+            <Link to='/' onClick={()=>setMenu("Home")} className={menu ==='Home'?'active':''}>Home</Link>
+            <a href='#explore-menu' onClick={()=>setMenu("Menu")} className={menu ==='Menu'?'active':''}>Menu</a>
+            <a href='#app-download' onClick={()=>setMenu("Mobile-app")} className={menu ==='Mobile-app'?'active':''}>Mobile-app</a>
+            <a href='#footer' onClick={()=>setMenu("Contact Us")} className={menu ==='Contact Us'?'active':''}>Contact Us</a>
         </ul>
         <div className="navbar-right">
             <img src={assets.search_icon} alt="" />
             <div className="navbar-search-icon">
-                <img src={assets.basket_icon} alt="" />
+                <Link to='/cart'><img src={assets.basket_icon} alt=""/></Link>
                 <div className="dot"></div>
             </div>
-            <button>Sign in</button>
+            <button onClick={()=>setShowLogin(true)}>Sign in</button>
         </div>
     </div>
   )
