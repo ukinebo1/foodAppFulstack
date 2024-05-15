4f57a65d26a755e723195262c238a806b32a9188
Ukinebo1 <perfectprecious2022@gmail.com>
2024-05-15 04:38:47 +0100

    Created and styled the footer components also added the mobile-app download components

--- foodApp/src/components/FoodDisplay/FoodDisplay.jsx
+++ foodApp/src/components/FoodDisplay/FoodDisplay.jsx
@@ -10,7 +10,10 @@ const FoodDisplay = ({category}) => {
         <h2>Top dishes near you</h2>
         <div className="food-display-list">
             {food_list.map((item, index)=>{
+              if(category==="All" || category===item.category){
                 return <FoodItem key={index} id={item._id} name={item.name} image={item.image} description={item.description} price={item.price} />
+              }
+                
             })}
         </div>
     </div>
--- foodApp/src/components/FoodItem/FoodItem.jsx
+++ foodApp/src/components/FoodItem/FoodItem.jsx
@@ -1,22 +1,31 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext'
 
 const FoodItem = ({id, name, image, description, price}) => {
-    const [itemCount, setItemCount]=useState(0)
+    // const [itemCount, setItemCount]=useState(0)
+    const {cartItems,addToCart,removeFromCart} = useContext(StoreContext);
 
 
   return (
     <div className='food-item'>
         <div className="food-item-img-container">
             <img src={image} className='food-item-image' alt="" />
-            {!itemCount 
+            {!cartItems[id] 
+            ? <img src={assets.add_icon_white} className='add' onClick={()=>addToCart(id)} /> 
+            : <div className='food-item-counter'>
+                <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" srcset="" />
+                <p>{cartItems[id]}</p>
+                <img src={assets.add_icon_green} alt=""  onClick={()=>addToCart(id)}/>
+                </div>}
+            {/* {!itemCount 
             ? <img src={assets.add_icon_white} className='add' onClick={()=>setItemCount(prev=>prev + 1)} /> 
             : <div className='food-item-counter'>
                 <img onClick={()=>setItemCount(prev=> prev - 1)} src={assets.remove_icon_red} alt="" srcset="" />
                 <p>{itemCount}</p>
                 <img src={assets.add_icon_green} alt=""  onClick={()=>setItemCount(prev=> prev + 1)}/>
-                </div>}
+                </div>} */}
         </div>
         <div className="food-item-info">
             <div className="food-item-name-rating">
